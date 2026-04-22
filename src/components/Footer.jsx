e89b9eff1e951d162eb76f140e65eb7eb2e57c1e
Maheswari Kumar <ksrimahi@gmail.com>
2026-04-22 21:22:33 +0530

    saving progress

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,19 @@
 export function Footer() {
-  return <h1>Footer Component</h1>;
+  return (
+    <div className="footer">
+      <div>
+        <p>© 2026 Company Name</p>
+      </div>
+
+      <div className="footer-links">
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+        <a href="#">Privacy</a>
+      </div>
+
+      <div>
+        <p>Social Things</p>
+      </div>
+    </div>
+  );
 }
