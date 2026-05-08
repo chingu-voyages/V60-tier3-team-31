6ead46ff1989e974399044b4c64cdac67526afa2
Maheswari Kumar <ksrimahi@gmail.com>
2026-05-08 21:43:22 +0530

    done setup landing page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,39 @@
+// export function Footer() {
+//   return (
+//     <div className="footer">
+//       <div>
+//         <p>© 2026 Company Name</p>
+//       </div>
+
+//       <div className="footer-links">
+//         <a href="#">About</a>
+//         <a href="#">Contact</a>
+//         <a href="#">Privacy</a>
+//       </div>
+
+//       <div>
+//         <p>Social Things</p>
+//       </div>
+//     </div>
+//   );
+// }
+
 export function Footer() {
   return (
-    <div className="footer">
-      <div>
-        <p>© 2026 Company Name</p>
-      </div>
+    <footer className="footer">
+      <h4>Team work</h4>
 
-      <div className="footer-links">
-        <a href="#">About</a>
-        <a href="#">Contact</a>
-        <a href="#">Privacy</a>
+      <div className="team-grid">
+        <div>
+          <span>Michael Okoro</span>
+          <span>Maheswari Kumar</span>
+          <span>Simbongile Mkhothi</span>
+        </div>
+        <div>
+          <span>Mustaqeem Chowdhury</span>
+          <span>Yusuf Mohsen</span>
+        </div>
       </div>
-
-      <div>
-        <p>Social Things</p>
-      </div>
-    </div>
+    </footer>
   );
 }
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/NavBar";
+import Hero from "../components/Hero";
+import QuoteSection from "../components/QuoteSection";
+
+export default function Landing() {
+    return (
+        <>
+        <Navbar />
+        <Hero />
+        <QuoteSection />
+        </>
+    )
+}
